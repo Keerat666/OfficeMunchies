9155806d220061f043834a03f57a3ef85c5ef02b
keerat666 <gurkeeratsondhi@gmail.com>
2021-01-14 04:14:43 +0530

    Day 0 commit

---
+++ models/users.js
@@ -0,0 +1,20 @@
+//Require Mongoose
+var mongoose = require('mongoose');
+//Define a schema
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+    full_name: { type: String, description: "Required Field", required: true },
+    id_created_at: { type: Date, default: Date.now() },
+    gender: { type: String, enum: ['Male', 'Female', 'Others'] },
+    phone_no: { type: String, unique: true, required: true, description: "Should be unique" },
+    email: { type: String, unique: true, required: true, description: "Should be unique" },
+    password: { type: String, required: true, description: "Should be unique" },
+    wallet: { type: Number, default: 100 },
+    emp_no: { type: String, required: true },
+    profile_picture: { type: String, default: "https://res.cloudinary.com/dwckgkzdz/image/upload/v1604757274/buddha_wyz1ek.jpg" },
+
+}, { collection: 'users_OM' });
+var UserModel = mongoose.model('users', userSchema);
+
+module.exports = UserModel;
