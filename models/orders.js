f569eae5baf5740a2a2512b5e9ea0df5128f556f
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2021-01-24 20:09:35 +0530

    Update orders.js

--- models/orders.js
+++ models/orders.js
@@ -9,7 +9,7 @@ var orderSchema = new Schema({
                 item_name: { type: String, description: "Required Field", required: true },
                 item_created_at: { type: Date, default: Date.now() },
                 item_availability: { type: String, enum: ['True', 'False'] },
-                item_description: { type: String, unique: true, required: true, description: "Should be unique" },
+                item_description: { type: String, required: true, description: "Should be unique" },
                 item_price: { type: Number },
                 main_image: { type: String, required: true },
                 cartStatus: { type: String, required: true },
