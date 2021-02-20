c5c45d1dd8a2c1d8de42f17793e0fefe2c477bb1
Gurkeerat Singh <gurkeeratsondhi@gmail.com>
2021-02-21 03:02:01 +0530

    Minor changes

--- middlewares/connection.js
+++ middlewares/connection.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 // var mongoDB = 'mongodb://localhost/soaps';
 // mongoose.connect(mongoDB, { useNewUrlParser: true });
 //Cloud Atlas : mongodb+srv://admin:PLgTOTRSbaapogZn@ascential.biggr.mongodb.net/Ascential_Dev?retryWrites=true&w=majority
-var conn = mongoose.connect('mongodb://admin:zllmBrsTuhgmhMa0@ascentialdb-shard-00-00.biggr.mongodb.net:27017,ascentialdb-shard-00-01.biggr.mongodb.net:27017,ascentialdb-shard-00-02.biggr.mongodb.net:27017/heroku_19h1zvk6?ssl=true&replicaSet=atlas-vntias-shard-0&authSource=admin&retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false })
+var conn = mongoose.connect('db connection string in here', { useNewUrlParser: true, useFindAndModify: false })
     .then((db) => {
         console.log('Database connected');
 
